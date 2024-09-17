fc93080940262c306edee44a12e86a0e667b47bf
axel <i@amusaev.ru>
2024-09-17 17:22:23 +0300

    init commit

---
+++ src/test/java/com/axel/clientcontactapi/ClientContactApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axel.clientcontactapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientContactApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
